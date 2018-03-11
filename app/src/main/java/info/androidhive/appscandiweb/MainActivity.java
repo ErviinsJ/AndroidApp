beeb0009e31d1096f2900e8cd80cf4fa57860951
ErviinsJ <ervinsjansons@gmail.com>
2018-03-11 21:42:14 +0200

    statistic activity

--- app/src/main/java/info/androidhive/appscandiweb/MainActivity.java
+++ app/src/main/java/info/androidhive/appscandiweb/MainActivity.java
@@ -2,17 +2,23 @@ package info.androidhive.appscandiweb;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.PorterDuff;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
 import android.widget.Button;
 import android.widget.EditText;
 
+import static android.R.attr.button;
 import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
 
+
     public void statistB(View view) {
+
         Intent intent = new Intent(this, StatisticActivity.class);
         startActivity(intent);
     }
@@ -25,3 +31,5 @@ public class MainActivity extends AppCompatActivity {
 
     }
 }
+
+
